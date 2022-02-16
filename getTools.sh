ee9dbb150c7315f3c75efd7a973f4ba67c0eff40
Victoria Galvan <9023257+ch4kra@users.noreply.github.com>
2022-02-15 22:02:02 -0500

    Added SecLists

--- getTools.sh
+++ getTools.sh
@@ -24,7 +24,7 @@ echo -e "${green}Installed Ruby.${clearColor}"
 
 # Install Chisel
 echo -e "${green}Getting Chisel.${clearColor}"
-sudo apt install chisel
+sudo apt install chisel -y
 
 # Install Evil-WinRM
 echo -e "${green}Installing Evil-WinRM.${clearColor}"
@@ -32,7 +32,7 @@ sudo gem install evil-winrm
 
 # Install Bloodhound
 echo -e "${green}Getting Bloodhound.${clearColor}"
-sudo apt install bloodhound
+sudo apt install bloodhound -y
 
 # Get WinPeas
 echo -e "${green}Cloning PEASS-ng${clearColor}"
@@ -40,15 +40,19 @@ git clone git@github.com:carlospolop/PEASS-ng.git
 
 # Install CrackMapExec
 echo -e "${green}Getting crackmapexec.${clearColor}"
-sudo apt install crackmapexec
+sudo apt install crackmapexec -y
 
 # Install sqlmap
 echo -e "${green}Getting sqlmap.${clearColor}"
-sudo apt install sqlmap
+sudo apt install sqlmap -y
 
-# Get Impacket Scripts (maybe)
+# Get Impacket Scripts
 echo -e "${green}Getting Impacket scripts.${clearColor}"
-sudo apt install impacket-scripts
+sudo apt install impacket-scripts -y
+
+# Get SecLists
+echo -e "${green}Getting SecLists.${clearColor}"
+sudo apt install seclists -y
 
 # Update Packages
 echo -e "${green}Removing unnecessary files..${clearColor}"
